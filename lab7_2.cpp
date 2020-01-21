31c958821b8b25628e13fe7b691d453834f66582
FALLENSLAYER007 <37149762+POOHPHE@users.noreply.github.com>
2020-01-21 15:01:39 +0700

    Add files via upload
    Lab 7 21/1/2020

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -5,6 +5,19 @@ using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
 
+string findGrade(int sc){
+	if(sc>90){
+			return "A";
+		}else if(sc>75){
+			return "B";
+		}else if(sc>60){
+			return "C";
+		}else if(sc>45){
+			return "D";
+		}else{
+			return "F";
+		}
+}
 int main(){
 	//Input the number of students
 	int N,i = 0;
@@ -18,8 +31,11 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin>>score[i];
+		
 		i++;
 	}
 	
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,40 @@
 //status = "ONE-NIGHT-STAND";
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	double age,h;
+	long double money;
+	string status;
+	cin>>age;
+	
+	if(age<=20){
+		cin>>h;
+		if(h<160){
+			status="UNFRIEND";
+		}else if(h<175){
+			status="FRIEND";
+		}else{
+			cin>>money;
+			if(money>69000000){
+				status="MARRIED";
+			}else{
+				status="ONE-NIGHT STAND";
+			}
+		}
+	}else if(age<30){
+		cin>>money;
+		
+		if(money>10000000){
+			status="BEST FRIEND";
+		}else{
+			status="UNFRIEND";
+		}
+		
+	}else{
+		status="UNFRIEND";
+	}
+	
+	
+	
+	cout<<status;
+}
